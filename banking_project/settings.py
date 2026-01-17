2f497675a4b276c3d8d16d4daa3e2731744311aa
Anamika Mishra <mishraanamika11a@gmail.com>
2026-01-17 22:38:27 +0530

    Ready for deployment

--- banking_project/settings.py
+++ banking_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,5 +122,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-ALLOWED_HOSTS = ["*"]
+
+import os
+
+# Static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Allow all hosts (will add specific domain later)
+ALLOWED_HOSTS = ['*']
